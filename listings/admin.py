8450feed9329e6b1eead766da8c77d3ef6476430
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-02-20 17:50:02 +0100

    modification of django admin area

--- listings/admin.py
+++ listings/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Listing
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'address', 'city', 'price', 'list_date')
+    list_filter = ('realtor', 'state')
+    list_editable = ('is_published',)
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'city', 'state', 'address', 'price')
+    list_per_page = 10
+
+
 # Register your models here.
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
 from .models import Realtor
+
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'is_mvp', 'phone', 'hire_date')
+    list_display_links = ('id', 'name')
+    list_editable = ('is_mvp',)
+    search_fields = ('name', 'email')
+    list_per_page = 10 
+
 # Register your models here.
-admin.site.register(Realtor)
+admin.site.register(Realtor, RealtorAdmin)
